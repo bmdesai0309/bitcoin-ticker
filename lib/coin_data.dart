0bd91344429d2aa2e095213780aaced8c749b854
bmdesai0309 <bmdesai1685@gmail.com>
2021-01-06 23:37:11 +0530

    add coinData class

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,26 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CoinData {
+
+  String btc;
+  String currencyName;
+
+  void getCoinData() async {
+    http.Response response =await http.get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=C9B3A00F-031F-4330-9AB6-1009E54E125E');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      print(decodedData);
+      return decodedData;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+}
+
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,5 +50,3 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
-
-class CoinData {}
